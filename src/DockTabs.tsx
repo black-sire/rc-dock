4c5a55d9438e06686d6c67fa7ade5ceeeaa0dd00
Shravan Kumar Karnati <shravank@truveta.com>
2023-01-02 08:39:13 -0800

    dropdown classes

--- src/DockTabs.tsx
+++ src/DockTabs.tsx
@@ -9,6 +9,7 @@ import {DockTabBar} from "./DockTabBar";
 import DockTabPane from "./DockTabPane";
 import {getFloatPanelSize} from "./Algorithm";
 import {WindowBox} from "./WindowBox";
+import {groupClassNames} from "./Utils";
 
 function findParentPanel(element: HTMLElement) {
   for (let i = 0; i < 10; ++i) {
@@ -329,6 +330,7 @@ export class DockTabs extends React.PureComponent<Props> {
             renderTabBar={this.renderTabBar}
             activeKey={activeId}
             onChange={this.onTabChange}
+            popupClassName={groupClassNames(group)}
       >
         {children}
       </Tabs>
